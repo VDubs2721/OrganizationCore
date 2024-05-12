7d5370fa21e3b590a5664a262b2e16af9913d298
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-12 01:03:03 -0400

    Add files via upload

---
+++ eventSelection.js
@@ -0,0 +1,225 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getDatabase, ref, set, child, get, remove, update} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+import {getStorage, ref as sref, uploadBytes, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js"
+// == Final ==
+// Header Package=============================================================================================================
+const firebaseConfig = {
+  apiKey: "AIzaSyDF5_FiAIvFJ4DAgRgXirWqzlsMzSBR5oA",
+  authDomain: "organization-manager-7fcb2.firebaseapp.com",
+  databaseURL: "https://organization-manager-7fcb2-default-rtdb.firebaseio.com",
+  projectId: "organization-manager-7fcb2",
+  storageBucket: "organization-manager-7fcb2.appspot.com",
+  messagingSenderId: "865663758325",
+  appId: "1:865663758325:web:8d02b101660f9d50a6babc"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+
+document.getElementById("sign-out").addEventListener('click', signOut);
+
+function signOut() {
+  sessionStorage.setItem("currentUser", "");
+}
+getDownloadURL(sref(storage, 'users/' + sessionStorage.getItem("currentUser")))
+.then((url) => {
+
+  // Or inserted into an <img> element
+  const img = document.getElementById('profile-pic');
+  img.setAttribute('src', url);
+});
+get(child(ref(db), 'users/' + sessionStorage.getItem("currentUser"))).then((snapshot) => {
+  if (!snapshot.val().creator)
+    document.getElementById("createClub").style.display = "none";
+
+});
+// End of Header Package================================================================================================
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Initialize Firebase
+
+// Initialize Realtime Database and get a reference to the service
+
+
+const container = document.getElementById('container');
+
+var snapshot = await get(child(ref(db), 'events/'));
+
+const snapshotArray = [];
+snapshot.forEach(childSnapshot => {
+  snapshotArray.push(childSnapshot);
+});
+
+
+// Iterate over the array using for...of loop
+for (const childSnapshot of snapshotArray) {
+  const eventId = childSnapshot.key;
+  //childSnapshot.child('username').val()
+  // Create the parent container div
+  const latestGridItem = document.createElement('div');
+  latestGridItem.classList.add('latest-grid-item');
+  latestGridItem.id = eventId;
+  await getDownloadURL(sref(storage, 'events/' + eventId))
+  .then((url) => {
+
+    // Or inserted into an <img> element
+    const img = document.createElement('img');
+    img.src = url;
+    latestGridItem.appendChild(img);
+
+  });
+  // Create and append the h2 element for name
+  const nameHeading = document.createElement('h2');
+  //const clubHeading = document.createElement('h3');
+  const dateHeading = document.createElement('h3');
+  const hr = document.createElement('hr');
+  const clubLink = document.createElement('a');
+  clubLink.href = "Organizations/organizationProfile.html";
+  clubLink.innerHTML = "Host Club";
+  const descriptionParagraph = document.createElement('p');
+  const buttonContainer = document.createElement('span');
+  //buttonContainer.id = eventId;
+  const firstButton = document.createElement('button');
+  firstButton.textContent = "Drive";
+  firstButton.id = "driver" + eventId;
+  const secondButton = document.createElement('button');
+  secondButton.textContent = "Carpool";
+  secondButton.id = "carpool" + eventId;
+  const info = document.createElement('a');
+  info.href = "Transit/driver.html";
+  const infoIcon = document.createElement('img');
+  infoIcon.src = "https://static-00.iconduck.com/assets.00/info-circle-icon-512x480-r5cstdbp.png";
+  infoIcon.classList.add("info");
+  info.appendChild(infoIcon);
+  buttonContainer.appendChild(secondButton);
+  buttonContainer.appendChild(firstButton);
+  buttonContainer.appendChild(info);
+
+  await get(child(ref(db), 'events/' + eventId)).then((snapshot) => {
+    if (snapshot.exists()) {
+      nameHeading.textContent = snapshot.val().name;
+      latestGridItem.appendChild(nameHeading);
+      dateHeading.textContent = snapshot.val().date;
+      latestGridItem.appendChild(dateHeading);
+      latestGridItem.appendChild(hr);
+      clubLink.id = eventId + "club" + snapshot.val().club;
+      latestGridItem.appendChild(clubLink);
+      descriptionParagraph.innerHTML = "Location: " + snapshot.val().location + "<br>Snacks: " + snapshot.val().snacks + "<br>Description: " + snapshot.val().description;
+      latestGridItem.appendChild(descriptionParagraph);
+      latestGridItem.appendChild(buttonContainer);
+    }
+  });
+  container.appendChild(latestGridItem);
+  document.getElementById(eventId).addEventListener("click", async function(event) {
+    // Access the id attribute of the target element
+    if (event.target.id == "") return;
+    const id = event.target.id.charAt(event.target.id.length - 1).toString();
+    if (event.target.id.includes("club")) {
+      sessionStorage.setItem("currentClub", id);
+      return;
+    }
+    var driverExists = false;
+    const driver = await get(child(ref(db), 'events/' + id + '/driver'));
+    if (driver.exists() && driver.val() != "") {
+      driverExists = true;
+    }
+    if (event.target.id.includes("driver")) {
+      if (driverExists) {
+        alert("There is already a driver for this event.");
+        return;
+      }
+      else {
+        await update(ref(db, 'events/' + id), {
+          driver: sessionStorage.getItem("currentUser")
+        });
+        await get(child(ref(db), 'users/' + sessionStorage.getItem("currentUser"))).then(async (snapshot) => {
+          // Get the current array data from the snapshot
+          var currentArray = snapshot.val().events || [];
+          // Iterate through the array items
+          currentArray.push(id);
+
+          // Set the modified array back to the database
+          await update(ref(db, "users/" + sessionStorage.getItem("currentUser")), {
+            events: currentArray
+          });
+        });
+      }
+      sessionStorage.setItem("currentEvent", id);
+      window.location.href = 'Transit/eventReceipt.html';
+    }
+    else if (event.target.id.includes("carpool")) {
+      if (!driverExists) {
+        alert("There is no driver for this event. Wait for a driver to sign up or sign up as a driver yourself.")
+        return;
+      }
+      await get(child(ref(db), 'events/' + id)).then(async (snapshot) => {
+        // Get the current array data from the snapshot
+        var currentArray = snapshot.val().carpoolers || [];
+        // Iterate through the array items
+        currentArray.push(sessionStorage.getItem("currentUser"));
+
+        // Set the modified array back to the database
+        await update(ref(db, "events/" + id + "/"), {
+          carpoolers: currentArray
+        });
+      });
+      await get(child(ref(db), 'users/' + sessionStorage.getItem("currentUser"))).then(async (snapshot) => {
+        // Get the current array data from the snapshot
+        var currentArray = snapshot.val().events || [];
+        // Iterate through the array items
+        currentArray.push(id);
+
+        // Set the modified array back to the database
+        await update(ref(db, "users/" + sessionStorage.getItem("currentUser")), {
+          events: currentArray
+        });
+      });
+      sessionStorage.setItem("currentEvent", id);
+      window.location.href = 'Transit/eventReceipt.html';
+    }
+
+  });
+
+}
+
+
+// document.getElementById("button").addEventListener("click", function(event) {
+//   event.preventDefault();
+//   if (localStorage.getItem("driver") !== null) {
+//     window.location.href = "Transit/eventReceipt.html";
+//   }
+//   else {
+//     localStorage.setItem("driver", localStorage.getItem("currentuser"));
+//     alert("Successfully signed up as a driver!");
+//     window.location.href = "Transit/carpoolHub.html";
+//   }
+// })
+
+/*
+const events = [
+  { name: "Event 1", date: "2024-04-10", location: "Location 1" },
+  { name: "Event 2", date: "2024-04-15", location: "Location 2" },
+  { name: "Event 3", date: "2024-04-20", location: "Location 3" }
+];
+
+function createEventButtons() {
+  const eventButtonsDiv = document.getElementById("eventButtons");
+
+  events.forEach(event => {
+    const button = document.createElement("button");
+    button.textContent = `${event.name} - ${event.date}`;
+    button.setAttribute("data-location", event.location);
+    button.addEventListener("click", function() {
+      const location = this.getAttribute("data-location");
+      alert(`You selected ${event.name}. Location: ${location}`);
+
+    });
+    eventButtonsDiv.appendChild(button);
+  });
+}
+
+
+window.addEventListener("load", createEventButtons);
+*/
---
+++ login.js
@@ -0,0 +1,145 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getDatabase, ref, set, child, get, remove, update} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+import {getStorage, ref as sref, uploadBytes} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js";
+// == Final ==
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDF5_FiAIvFJ4DAgRgXirWqzlsMzSBR5oA",
+  authDomain: "organization-manager-7fcb2.firebaseapp.com",
+  databaseURL: "https://organization-manager-7fcb2-default-rtdb.firebaseio.com",
+  projectId: "organization-manager-7fcb2",
+  storageBucket: "organization-manager-7fcb2.appspot.com",
+  messagingSenderId: "865663758325",
+  appId: "1:865663758325:web:8d02b101660f9d50a6babc"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
+
+const creatorSubmit = document.getElementById('asCreator');
+
+// Flags to track which button was clicked
+let asCreator = false;
+
+// Add click event listeners to the submit buttons
+creatorSubmit.addEventListener('click', () => {
+  asCreator = true;
+});
+
+/*const sn = get(child(ref(db), "users/1")).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(!snapshot.val().creator);
+    }
+});*/
+
+// the below is not used but for reference
+document.getElementById("signupform").addEventListener("submit", async function(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("signupusername").value.trim();
+  const name = document.getElementById("signupname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("signuppassword").value.trim();
+  const address = document.getElementById("address").value.trim();
+  const phone = document.getElementById("phonenumber").value.trim();
+  const imageUploadInput = document.getElementById('imageUpload');
+
+
+  let usernameAlreadyExists = false;
+  var snapshot = await get(child(ref(db), 'users/'));
+  snapshot.forEach((childSnapshot) => {
+    if (childSnapshot.child('username').val() == username)
+      usernameAlreadyExists = true;
+  });
+  if (usernameAlreadyExists) {
+    alert("Username already exists");
+    return;
+  }
+  let i = 1;
+  let done = false;
+  while (!done) {
+    snapshot = await get(child(ref(db), 'users/' + i));
+    if (snapshot.exists()) {
+      i++;
+    } else {
+      await set(ref(db, 'users/' + i), {
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+        address: address,
+        phone: phone,
+        creator: asCreator
+      });
+      done = true;
+    }
+  }
+  const storageRef = sref(storage, 'users/' + i);
+  const file = imageUploadInput.files[0];
+  // 'file' comes from the Blob or File API
+  await uploadBytes(storageRef, file);
+  //uploadBytes(storageRef, imageUploadInput.files[0]);
+  sessionStorage.setItem("currentUser", i);
+  // while (localStorage.getItem("user" + i) !== null) {
+  //   i++;
+  // }
+
+  //document.getElementById("result").innerHTML = localStorage.getItem("user1");
+
+  //localStorage.clear();
+  // Assume AJAX call to send login info to server and save in database
+  const urlParams = new URLSearchParams(window.location.search);
+  //Redirect to page that brought user to login page
+  window.location.href = urlParams.get('redirect') ? urlParams.get('redirect') : 'organizationCore.html';
+});
+
+document.getElementById("signinform").addEventListener("submit", async function(event) {
+  event.preventDefault(); 
+
+  const username = document.getElementById("signinusername").value.trim();
+  const password = document.getElementById("signinpassword").value.trim();
+
+  var passwordCorrect = false;
+  var i = "";
+  var snapshot = await get(child(ref(db), 'users/'));
+  snapshot.forEach((childSnapshot) => {
+    if (childSnapshot.child('username').val() == username && childSnapshot.child('password').val() == password) {
+      passwordCorrect = true;
+      i = childSnapshot.key;
+    }
+  });
+
+  if (!passwordCorrect) {
+    alert("Invalid username or password");
+    return;
+  }
+  else {
+    sessionStorage.setItem("currentUser", i);
+    const urlParams = new URLSearchParams(window.location.search);
+    window.location.href = urlParams.get('redirect') ? urlParams.get('redirect') : 'organizationCore.html';
+
+    //document.getElementById("result").innerHTML = localStorage.getItem("user1");
+
+    //localStorage.clear();
+    // Assume AJAX call to send login info to server and save in database
+    // Redirect to another page after successful login
+    //window.location.href = "/result.html?driver=" + name; // Redirect to event selection page
+  }
+
+});
